6ae499d21edf60e712982d00368a594adaa86f1c
Mikhail Abramov <beavisabra@yandex.ru>
2017-11-29 11:41:55 +0300

    Добавил директора в игру

--- game.cpp
+++ game.cpp
@@ -1,7 +1,8 @@
 #include "Lib\\TXLib.h"
 #include "Lib\\Bomzh.cpp"
+#include "Lib\\Director.cpp"
 
- void scene1(Bomzh b/*, Director d*/)
+ void scene1(Bomzh b, Director d, Point p)
  {
     int x = 700;
     int y = 0;
@@ -13,6 +14,14 @@
         txRectangle(0, 0, 1200, 900);
         moveBomzh(&b);
         drawBomzh(b);
+
+        txSetColor(TX_RED, 3);
+        txCircle(p.x, p.y, 5);
+        txCircle(p.x1, p.y1, 5);
+        txCircle(p.x2, p.y2, 5);
+
+        drawDirector(d);
+        moveDirector(&d, &p);
 
         txSetColor(TX_RED);
         txSetFillColor(TX_RED);
@@ -70,48 +79,82 @@
         }
 
         txSleep(10);
-
-         //moveDirector(&d);
-         //drawDirector(d);
     }
 
  }
 
- int main()
- {
+int main()
+{
 
-     txCreateWindow (1090, 654);
-     txBegin();
+    txCreateWindow (1090, 654);
+    txBegin();
 
-     //Director d;
-     //d.x = 50;
-     //...
-     //d.picRight = ...
+    //Director d;
+    //d.x = 50;
+    //...
+    //d.picRight = ...
 
-     Bomzh b;
-     b.x = 400;
-     b.y = 300;
-     b.width = 54;
-     b.height = 100;
-     b.speed = 1;
-     b.manyframeRight = 2;
-     b.manyframeLeft = 2;
-     b.PointStartX1 = 27;
-     b.PointStartX2 = 24;
-     b.PointStartY = 74;
-     b.manyframeUp = 4;
-     b.manyframeDown = 4;
-     b.direction = 0;
-     b.frame = 0;
-     b.frameTimer = 0;
-     b.picDown = txLoadImage("IMG\\Men\\Homeless\\HomelessDown.bmp");
-     b.picUp = txLoadImage("IMG\\Men\\Homeless\\HomelessUp.bmp");
-     b.picLeft = txLoadImage("IMG\\Men\\Homeless\\HomelessLeft.bmp");
-     b.picRight = txLoadImage("IMG\\Men\\Homeless\\HomelessRight.bmp");
+    Bomzh b;
+    b.x = 400;
+    b.y = 300;
+    b.width = 54;
+    b.height = 100;
+    b.speed = 1;
+    b.manyframeRight = 2;
+    b.manyframeLeft = 2;
+    b.PointStartX1 = 27;
+    b.PointStartX2 = 24;
+    b.PointStartY = 74;
+    b.manyframeUp = 4;
+    b.manyframeDown = 4;
+    b.direction = 0;
+    b.frame = 0;
+    b.frameTimer = 0;
+    b.picDown = txLoadImage("IMG\\Men\\Homeless\\HomelessDown.bmp");
+    b.picUp = txLoadImage("IMG\\Men\\Homeless\\HomelessUp.bmp");
+    b.picLeft = txLoadImage("IMG\\Men\\Homeless\\HomelessLeft.bmp");
+    b.picRight = txLoadImage("IMG\\Men\\Homeless\\HomelessRight.bmp");
 
-     scene1(b/*, d*/);
-     txDeleteDC(b.picDown);
-     txDeleteDC(b.picUp);
-     txDeleteDC(b.picLeft);
-     txDeleteDC(b.picRight);
- }
+
+    Director d;
+    d.x = 300;
+    d.y = 200;
+    d.speed = 10;
+    d.width = 54;
+    d.height = 86;
+    d.speed = 2;
+    d.manyframeRight = 3;
+    d.manyframeLeft = 3;
+    d.PointStartX1 = 27;
+    d.PointStartX2 = 24;
+    d.PointStartY = 74;
+    d.manyframeUp = 5;
+    d.manyframeDown = 5;
+    d.direction = 0;
+    d.frame = 0;
+    d.frameTimer = 0;
+    d.picDown = txLoadImage ("IMG\\Men\\Homeless\\HomelessDown.bmp");
+    d.picUp = txLoadImage ("IMG\\Men\\Homeless\\HomelessUp.bmp");
+    d.picLeft = txLoadImage ("IMG\\Men\\Homeless\\HomelessLeft.bmp");
+    d.picRight = txLoadImage ("IMG\\Men\\Homeless\\HomelessRight.bmp");
+    //d.exit_time = 100000;
+
+    Point p;
+    p.x = 100;
+    p.y = 100;
+    p.x1 = 200;
+    p.y1 = 100;
+    p.x2 = 150;
+    p.y2 = 150;
+    p.nomerPoint = 0;
+
+    scene1(b, d, p);
+    txDeleteDC(b.picDown);
+    txDeleteDC(b.picUp);
+    txDeleteDC(b.picLeft);
+    txDeleteDC(b.picRight);
+    txDeleteDC(d.picDown);
+    txDeleteDC(d.picUp);
+    txDeleteDC(d.picLeft);
+    txDeleteDC(d.picRight);
+}
