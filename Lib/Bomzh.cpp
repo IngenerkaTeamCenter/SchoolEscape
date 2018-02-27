307ce4ef03fc25573cbb1f1cd29cdcd0639d626a
MadDanX <31888819+MadDanX@users.noreply.github.com>
2018-02-27 17:08:47 +0400

    dfdhdh-2

--- Lib/Bomzh.cpp
+++ Lib/Bomzh.cpp
@@ -1,6 +1,24 @@
 #pragma once
 
 #include "TXLib.h"
+#include "consmenu.cpp"
+
+struct CrashZone
+{
+    int x1, y1;
+    int x2, y2;
+
+};
+
+bool intersect(int al, int ar, int bl, int br)
+{
+    return ((al <= bl && bl <= ar) || (bl <= al && al <= br));
+}
+
+bool intersect(CrashZone a, CrashZone bi)
+{
+    return (intersect(a.x1, a.x2, bi.x1, bi.x2) && intersect(a.y1, a.y2, bi.y1, bi.y2));
+}
 
 struct Bomzh
 {
@@ -22,25 +40,26 @@ struct Bomzh
     HDC picRight;
     int frame;
     int frameTimer;
+    CrashZone crash;
 };
 
 void drawBomzh(Bomzh b)
 {
     if (b.direction == DIRECTION_DOWN)
     {
-        txTransparentBlt(txDC(), b.x - b.PointStartX1 - abs_x, b.y - b.PointStartY, b.width, b.height, b.picDown, b.frame * 65, 0, RGB(255, 255, 255));
+        txTransparentBlt(txDC(), b.x - b.PointStartX1 /*- abs_x*/, b.y - b.PointStartY, b.width, b.height, b.picDown, b.frame * 65, 0, RGB(255, 255, 255));
     }
     else if (b.direction == DIRECTION_UP)
     {
-        txTransparentBlt(txDC(), b.x - b.PointStartX1 - abs_x, b.y - b.PointStartY, b.width, b.height, b.picUp, b.frame * 65, 0, RGB(255, 255, 255));
+        txTransparentBlt(txDC(), b.x - b.PointStartX1 /*- abs_x*/, b.y - b.PointStartY, b.width, b.height, b.picUp, b.frame * 65, 0, RGB(255, 255, 255));
     }
     else if (b.direction == DIRECTION_LEFT)
     {
-        txTransparentBlt(txDC(), b.x - b.PointStartX2 - abs_x, b.y - b.PointStartY, b.width - 6, b.height, b.picLeft, b.frame * 64, 0, RGB(255, 255, 255));
+        txTransparentBlt(txDC(), b.x - b.PointStartX2 /*- abs_x*/, b.y - b.PointStartY, b.width - 6, b.height, b.picLeft, b.frame * 64, 0, RGB(255, 255, 255));
     }
     else if (b.direction == DIRECTION_RIGHT)
     {
-        txTransparentBlt(txDC(), b.x - b.PointStartX2 - abs_x, b.y - b.PointStartY, b.width - 6, b.height, b.picRight, b.frame * 64, 0, RGB(255, 255, 255));
+        txTransparentBlt(txDC(), b.x - b.PointStartX2 /*- abs_x*/, b.y - b.PointStartY, b.width - 6, b.height, b.picRight, b.frame * 64, 0, RGB(255, 255, 255));
     }
 
     //abs_x = b.x - 300;
@@ -96,4 +115,12 @@ void moveBomzh(Bomzh* b)
             }
         }
     }
+}
+
+void fillCrashZone(Bomzh* b)
+{
+    b->crash.x1 = b->x - 27;
+    b->crash.y1 = b->y - 12;
+    b->crash.x2 = b->x + 27;
+    b->crash.y2 = b->y + 12;
 }
--- Lib/Pitek.cpp
+++ Lib/Pitek.cpp
@@ -6,9 +6,18 @@ struct Pitek
     int width;
     int height;
     HDC picDown;
+    CrashZone crash;
 };
 
 void drawPitek(Pitek pi)
 {
     txTransparentBlt(txDC(),  pi.x, pi.y, pi.width, pi.height, pi.picDown, 0, 0, RGB(255, 255, 255));
+}
+
+void fillCrashZone(Pitek* pi)
+{
+    pi->crash.x1 = pi->x - 27;
+    pi->crash.y1 = pi->y - 12;
+    pi->crash.x2 = pi->x + 27;
+    pi->crash.y2 = pi->y + 12;
 }
