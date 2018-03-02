9ed8c0b767fabd0e186f543d92abe117ec157bad
MadDanX <31888819+MadDanX@users.noreply.github.com>
2018-03-02 16:19:35 +0400

    ЧТо то сделал.

--- Lib/Pitek.cpp
+++ Lib/Pitek.cpp
@@ -1,5 +1,6 @@
 #include "TXLib.h"
 
+
 struct Pitek
 {
     int x, y;
@@ -11,13 +12,13 @@ struct Pitek
 
 void drawPitek(Pitek pi)
 {
-    txTransparentBlt(txDC(),  pi.x, pi.y, pi.width, pi.height, pi.picDown, 0, 0, RGB(255, 255, 255));
+    txTransparentBlt(txDC(),  pi.x - pi.width / 2, pi.y - pi.height / 2, pi.width, pi.height, pi.picDown, 0, 0, RGB(255, 255, 255));
 }
 
 void fillCrashZone(Pitek* pi)
 {
-    pi->crash.x1 = pi->x - 27;
-    pi->crash.y1 = pi->y - 12;
-    pi->crash.x2 = pi->x + 27;
-    pi->crash.y2 = pi->y + 12;
+    pi->crash.x1 = pi->x - 90;
+    pi->crash.y1 = pi->y - 29;
+    pi->crash.x2 = pi->x + 90;
+    pi->crash.y2 = pi->y + 40;
 }
